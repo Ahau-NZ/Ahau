96e916bf4a4da7f148c3089a6165d8912175fc13
mixmix <whimful@gmail.com>
2019-10-24 16:36:41 +1300

    update ssb-config for ipv6 context

--- ssb.config.js
+++ ssb.config.js
@@ -1,7 +1,7 @@
 const Config = require('ssb-config/inject')
 
 module.exports = Config('ssb-ahau', {
-  port: 8009,
+  port: 8087,
   caps: {
     shs: 'LftKJZRB4nbBRnlJuFteWG9AP+gGboVEhibx016bR0s='
   }
