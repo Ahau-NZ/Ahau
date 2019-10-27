ab3866bbbd6b425ac56b00168a79fa8d0cf937b1
mixmix <whimful@gmail.com>
2019-10-27 23:37:51 +1300

    add 5second cache on getProfile!

--- graphql/ssb/profile.js
+++ graphql/ssb/profile.js
@@ -1,8 +1,16 @@
 const { isMsg } = require('ssb-ref')
 
+const cache = {}
+const TIME_VALID = 5e3 // time cache is valid for
+
 module.exports = function getProfile (sbot, id, cb) {
   if (!isMsg(id)) {
     cb(new Error('profile query expected %msgId, got ' + id))
+  }
+
+  if (cache[id] && cache[id].time + TIME_VALID > Date.now()) {
+    console.log('serving the old value!')
+    return cb(null, cache[id].state)
   }
 
   sbot.profile.get(id, (err, profile) => {
@@ -16,6 +24,12 @@ module.exports = function getProfile (sbot, id, cb) {
     sbot.get(id, (_, { author }) => {
       state.id = id // TODO change ssb-profile to do this
       state.tiaki = author
+
+      cache[id] = {
+        time: Date.now(),
+        state
+      }
+
       cb(null, state)
     })
     // >>>>
