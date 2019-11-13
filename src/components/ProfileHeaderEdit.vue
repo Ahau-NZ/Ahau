b985c908161211e7b63bcd9356a19425e452e801
luandro <luandro@gmail.com>
2019-11-13 20:21:07 -0200

    minor styling fixes

--- src/components/ProfileHeaderEdit.vue
+++ src/components/ProfileHeaderEdit.vue
@@ -2,9 +2,13 @@
   <v-container class="full-width my-0 py-0">
     <v-row v-if="!updatingHeader" class="header-bg">
       <v-img :src="headerImage ? headerImage.uri : ''" min-width="100%" height="35vh"/>
-      <v-btn @click="toggleUpdateHeader" class="edit-header-button" tile color="grey">
-        <v-icon>mdi-pencil</v-icon> Edit header
-      </v-btn>
+      <div class="header"></div>
+      <div @click="toggleUpdateHeader" class="edit-header">
+        <v-btn fab color="white">
+          <v-icon class="black--text">mdi-pencil</v-icon>
+        </v-btn>
+        <span class="white--text pl-4 title">Upload header photo</span>
+      </div>
     </v-row>
     <v-row v-if="updatingHeader" class="header-edit">
       <v-image-input
@@ -29,10 +33,12 @@
 
     <v-row class="avatar">
       <Avatar :image="avatarImage" :alt="preferredName" />
-      <v-btn v-if="!updatingAvatar" class="toggle" fab color="grey" @click="toggleUpdateAvatar">
-        <v-icon>mdi-camera</v-icon>
-      </v-btn>
-
+      <div class="avatar-image">
+        <v-btn v-if="!updatingAvatar" class="toggle" fab color="white" @click="toggleUpdateAvatar">
+          <v-icon class="black--text">mdi-camera</v-icon>
+        </v-btn>
+        <span class="caption pt-4">Upload profile photo</span>
+      </div>
       <v-container v-if="updatingAvatar" class="editor">
         <v-image-input
           v-model="newAvatar"
@@ -151,72 +157,111 @@ function dataURLtoFile (dataurl, filename) {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-  .full-width {
-    max-width: 100%
-  }
-  .super-z {
-    z-index: 998;
-  }
-  .header-bg {
-    background: linear-gradient(45deg, hsl(0, 6%, 37.1%) 12%, transparent 0, transparent 88%, hsl(0, 6%, 37.1%) 0), linear-gradient(135deg, transparent 37%, hsl(13.5, 4%, 31%) 0, hsl(13.5, 4%, 31%) 63%, transparent 0), linear-gradient(45deg, transparent 37%, hsl(0, 6%, 37.1%) 0, hsl(0, 6%, 37.1%) 63%, transparent 0), hsl(0, 5.2%, 27.6%);
-    background-size: 50px 50px;
-  }
-  .header-edit {
-    height: 64vh;
-    margin-bottom: calc(35vh - 64vh);
-    background: rgba(0,0,0,.6);
-    z-index: 999;
-  }
-  .edit-header-button {
-    cursor: pointer;
-    position: absolute;
-    top: 30vh;
-    right: 5vw;
-    z-index: 999;
-  }
-  .handle-header-buttons {
-    position: relative;
-    top: -15vh;
-    right: -55%;
-    z-index: 999;
-    display: flex;
-    flex-flow: row nowrap;
-    justify-content: space-around;
-    width: 300px;
-  }
-  .avatar {
-    position: relative;
-    top: -18.75vh;
-    left: calc(50% - 450px - 64px);
-    margin-bottom: -25vh;
-    width: 25vh;
+.full-width {
+  max-width: 100%;
+}
+.super-z {
+  z-index: 998;
+}
+.header-bg {
+  background: linear-gradient(
+      45deg,
+      hsl(0, 6%, 37.1%) 12%,
+      transparent 0,
+      transparent 88%,
+      hsl(0, 6%, 37.1%) 0
+    ),
+    linear-gradient(
+      135deg,
+      transparent 37%,
+      hsl(13.5, 4%, 31%) 0,
+      hsl(13.5, 4%, 31%) 63%,
+      transparent 0
+    ),
+    linear-gradient(
+      45deg,
+      transparent 37%,
+      hsl(0, 6%, 37.1%) 0,
+      hsl(0, 6%, 37.1%) 63%,
+      transparent 0
+    ),
+    hsl(0, 5.2%, 27.6%);
+  background-size: 50px 50px;
+}
+.header {
+  min-height: 35vh;
+  background: grey;
+  width: 100%;
+  margin-bottom: -35vh;
+  position: relative;
+  top: -35vh;
+  opacity: 0.6;
+}
+.header-edit {
+  height: 64vh;
+  margin-bottom: calc(35vh - 64vh);
+  background: rgba(0, 0, 0, 0.6);
+  z-index: 999;
+}
+.edit-header {
+  cursor: pointer;
+  position: absolute;
+  top: 25vh;
+  right: 5vw;
+  z-index: 999;
+}
+.handle-header-buttons {
+  position: relative;
+  top: -15vh;
+  right: -55%;
+  z-index: 999;
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: space-around;
+  width: 300px;
+}
+.avatar {
+  position: relative;
+  top: -18.75vh;
+  left: calc(50% - 400px - 64px);
+  margin-bottom: -25vh;
+  width: 25vh;
+}
+.avatar-image {
+  width: 25vh;
+  margin-bottom: -25vh;
+  background: rgba(100, 100, 100, 0.8);
+  position: relative;
+  top: -25vh;
+  border-radius: 12.5vh;
+  display: flex;
+  flex-flow: column;
+  align-content: center;
+  align-items: center;
+  justify-content: center;
+}
 
-    .toggle {
-      cursor: pointer;
-      position: absolute;
-      top: 17vh;
-      left: 1vw;
-      z-index: 999;
-    }
-    .editor {
-      position: absolute;
-      background: rgba(0,0,0,.8);
-      height: 460px;
-      width: 610px;
-      z-index: 998;
-      padding: 50px;
-
-      .actions {
-        position: relative;
-        top: -5vh;
-        left: -3vw;
-        left: 0;
-        z-index: 999;
-        display: flex;
-        flex-flow: row nowrap;
-        justify-content: space-around;
-        width: 300px;
-      }
-    }
-  }
+.toggle {
+  cursor: pointer;
+  z-index: 999;
+}
+.editor {
+  position: absolute;
+  background: rgba(0, 0, 0, 0.8);
+  height: 460px;
+  width: 610px;
+  z-index: 998;
+  padding: 50px;
+}
+.actions {
+  position: relative;
+  top: -5vh;
+  left: -3vw;
+  left: 0;
+  z-index: 999;
+  display: flex;
+  flex-flow: row nowrap;
+  justify-content: space-around;
+  width: 300px;
+}
 </style>
