ea692a4c9420abf778f6b2ae4bff255ab2b94ff0
chereseeriepa <cherese.eriepa@outlook.com>
2019-11-14 07:28:09 +1300

    node component draws its own circle

--- src/components/Tree/Node.vue
+++ src/components/Tree/Node.vue
@@ -1,26 +1,17 @@
 <template>
-  <circle :r="radius">
+  <circle :r="settings.radius" @click="onClick">
   </circle>
 </template>
 <script>
 /* es-lint disable */
 
 export default {
-  components: {
-
-  },
   props: ['node'],
-  mounted () {
-    console.log(this.node.id + " " + this.node.x + "," + this.node.y)
-  },
   data () {
     return {
-      open: false
-    }
-  },
-  computed: {
-    radius () {
-      return this.node.radius
+      settings: {
+        radius: 20
+      }
     }
   },
   methods: {
