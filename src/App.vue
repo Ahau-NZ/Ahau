bf8c7aff46714d7bcb24944d88ec80d9605aebcc
Ian Tairea <iantairea@Ians-MBP-3.fritz.box>
2019-12-08 18:45:44 +1300

    - added custom backgrounds - added private record ui

--- src/App.vue
+++ src/App.vue
@@ -22,6 +22,19 @@ export default {
   },
   components: {
     Appbar
+  },
+  // this watch add class to body depending on the route clicked. used for changing body backgrounds, unique to each route.
+  watch: {
+    $route: {
+      handler (to, from) {
+        const body = document.getElementsByTagName('body')[0]
+        if (from !== undefined) {
+          body.classList.remove('page--' + from.name.toLowerCase())
+        }
+        body.classList.add('page--' + to.name.toLowerCase())
+      },
+      immediate: true
+    }
   }
 }
 </script>
@@ -30,4 +43,34 @@ export default {
   a {
     text-decoration:  none;
   }
+
+  //remove default vuetify dark theme background
+  .v-application {
+    background: none !important;
+  }
+
+  //custom backgrounds per route. see above 'watcher'
+  .page--login {
+    background: url(./assets/niho.svg);
+    background-color: #303030;
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-position-y: -110%;
+  }
+  .page--whakapapaindex {
+    background: url(./assets/niho.svg);
+    background-color: #303030;
+    background-repeat: no-repeat;
+    background-position-x: 150%;
+    // background-size: cover;
+  }
+  .page--whakapapashow {
+    background: url(./assets/niho.svg);
+    background-color: #303030;
+    background-position-x: -300px;
+    // background-repeat: no-repeat;
+    // background-size: cover;
+  }
+  .page--personshow {background-color: pink;}
+
 </style>
