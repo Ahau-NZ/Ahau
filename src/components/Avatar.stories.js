469c4aa8e809ab2387c094a255eb72e526452da5
chereseeriepa <cherese.eriepa@outlook.com>
2020-01-16 09:17:00 +1300

    added storybook test cases to Avatar and AvatarGroup

--- src/components/Avatar.stories.js
+++ src/components/Avatar.stories.js
@@ -26,3 +26,38 @@ export const Small = () => ({
   }),
   components: { Avatar }
 })
+
+export const Koro = () => ({
+  template: `<Avatar gender="male" bornAt="1950-01-01"/>`,
+  components: { Avatar }
+})
+
+export const Kuia = () => ({
+  template: `<Avatar gender="female" bornAt="1950-01-01"/>`,
+  components: { Avatar }
+})
+
+export const Tane = () => ({
+  template: `<Avatar gender="male" bornAt="1980-01-01"/>`,
+  components: { Avatar }
+})
+
+export const Wahine = () => ({
+  template: `<Avatar gender="female" bornAt="1980-01-01"/>`,
+  components: { Avatar }
+})
+
+export const Tama = () => ({
+  template: `<Avatar gender="male" bornAt="2010-01-01"/>`,
+  components: { Avatar }
+})
+
+export const Kotiro = () => ({
+  template: `<Avatar gender="female" bornAt="2010-01-01"/>`,
+  components: { Avatar }
+})
+
+export const Unknown = () => ({
+  template: `<Avatar gender="unknown"/>`,
+  components: { Avatar }
+})
