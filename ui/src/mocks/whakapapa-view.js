bb32c54f234e8cab00066623a5f42bb7db6aaf8a
luandro <luandro@gmail.com>
2020-02-12 20:19:28 -0300

    added WhakapapaViewDialog story and whakapapa-view mocks

---
+++ ui/src/mocks/whakapapa-view.js
@@ -0,0 +1,7 @@
+export const viewMinimum = {
+  name: "Smith's family",
+  description: "The family for the Smith's family",
+  image:
+    'https://m.media-amazon.com/images/M/MV5BNTczMzk1MjU1MV5BMl5BanBnXkFtZTcwNDk2MzAyMg@@._V1_UY317_CR2,0,214,317_AL_.jpg',
+  focus: '$someId'
+}
