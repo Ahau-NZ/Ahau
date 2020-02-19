5220c959eb16ab65d5f566bfe66c7730084ac6b3
chereseeriepa <cherese.eriepa@outlook.com>
2020-02-20 10:36:34 +1300

    removed date-fix from tree-helpers

--- ui/src/lib/tree-helpers.js
+++ ui/src/lib/tree-helpers.js
@@ -45,26 +45,11 @@ function hydrate (node, flatStore) {
     })
   }
 
-  // TODO: see fixDate function below
-  if (output.bornAt) {
-    output.bornAt = fixDate(output.bornAt)
-  }
-  if (output.diedAt) {
-    output.diedAt = fixDate(output.diedAt)
-  }
-
   if (output.parents) {
     output.siblings = []
     output.parents = output.parents.map(profileId => {
       // look up the full record to replace the profileId
       var profile = flatStore[profileId]
-      // TODO: see fixDate function below
-      if (profile.bornAt) {
-        profile.bornAt = fixDate(profile.bornAt)
-      }
-      if (profile.diedAt) {
-        profile.diedAt = fixDate(profile.diedAt)
-      }
 
       if (profile.children) {
         const currentSiblings = profile.children.map(d => {
@@ -82,13 +67,6 @@ function hydrate (node, flatStore) {
   if (output.siblings) {
     output.siblings = output.siblings.map(profileId => {
       var profile = flatStore[profileId]
-      // TODO: see fixDate function below
-      if (profile.bornAt) {
-        profile.bornAt = fixDate(profile.bornAt)
-      }
-      if (profile.diedAt) {
-        profile.diedAt = fixDate(profile.diedAt)
-      }
       return profile
     })
   }
@@ -96,31 +74,8 @@ function hydrate (node, flatStore) {
   if (output.partners) {
     output.partners = output.partners.map(profileId => {
       var profile = flatStore[profileId]
-      // TODO: see fixDate function below
-      if (profile.bornAt) {
-        profile.bornAt = fixDate(profile.bornAt)
-      }
-      if (profile.diedAt) {
-        profile.diedAt = fixDate(profile.diedAt)
-      }
       return profile
     })
   }
   return output
-}
-
-// TODO: fix this Date hack
-// https://app.asana.com/0/1139954823432348/1155904273421465/f
-// the Date scalar we're using with graphql doesn't currently allow `null` as a valid date.
-// this can be fixed, but for the moment we chose a specific date in the past to encode `null`
-//
-// files invovled:
-// - src/lib/tree-helpers.js
-// - @ssb-graphql/profile -> get-profile.js
-function fixDate (date) {
-  if (date[0] === '-') {
-    return null
-  }
-  date = date.slice(0, 10)
-  return date
 }
