8db47ce15317848a05d04c36ac780f7b8fdb97f9
Ben <jamin.tairea@gmail.com>
2020-03-11 13:16:12 +1300

    updated tableIcon

--- ui/src/components/button/TableButton.vue
+++ ui/src/components/button/TableButton.vue
@@ -2,8 +2,8 @@
   <v-tooltip bottom>
       <template v-slot:activator="{ on }">
          <v-btn v-on="on" @click="toggle()" fab small color="blue-grey">
-           <v-icon v-if="this.tableIcon" light >mdi-table</v-icon>
-           <v-icon v-esle light >mdi-file-tree</v-icon>
+           <v-icon  v-if="tableIcon" light >mdi-table</v-icon>
+           <v-icon v-else light >mdi-file-tree</v-icon>
          </v-btn>
       </template>
       <span>registry</span>
