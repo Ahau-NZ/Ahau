08244fd4f2a6a93988a693565c5c20c176f0b4e1
chereseeriepa <cherese.eriepa@outlook.com>
2020-04-21 10:30:48 +1200

    can delete child nodes

--- ui/src/store.js
+++ ui/src/store.js
@@ -32,42 +32,69 @@ export default new Vuex.Store({
   }
 })
 
+/*
+  searches through the nestedWhakapap to find
+  the profile by id and updates their personal
+  details but not their children
+*/
 function updateNode (nestedWhakapapa, node) {
+  // if the nestedWhakapapa has no value
+  // then we can search it
   if (!nestedWhakapapa) return null
+
+  // if the nestedWhakapapa matches the node we are
+  // looking for, then look no further
   if (nestedWhakapapa.id === node.id) {
+    // update its value
     nestedWhakapapa = node
     return nestedWhakapapa
   }
 
+  // if this nestedWhakapap isnt the one we are looking for,
+  // try searching its children
   nestedWhakapapa.children = nestedWhakapapa.children.map(child => {
-    var found = updateNode(child, node)
-    if (found) return found
-
-    return child
+    // do the same for each child
+    return updateNode(child, node) // will either return a changed value or the same one
   })
 
   return nestedWhakapapa
 }
 
+/*
+  searches through the nestedWhakapapa to find
+  the profile by id and removes them and all their decendants
+*/
 function deleteNode (nestedWhakapapa, id) {
+  // if the nestedWhakapapa has no value
+  // then we can search it
   if (!nestedWhakapapa) return null
+
+  // if the nestedWhakapapa matches the id we are
+  // looking for, then look no further
   if (nestedWhakapapa.id === id) {
     // found the node we need to delete
+    // so returning null will set its value to null
     return null
   }
 
-  var childToDelete = null
+  // if this nestedWhakapap isnt the one we are looking for,
+  // try searching its children
+
+  var childIndex = -1
 
   nestedWhakapapa.children.some((child, i) => {
     var node = deleteNode(child, id)
-    if (!node) {
-      childToDelete = i
+    console.log('node', node)
+    if (node === null) {
+      childIndex = i
       return true
     }
   })
 
-  if (childToDelete) {
-    nestedWhakapapa.children.splice(childToDelete, 1)
+  // if an index was set
+  if (childIndex > -1) {
+    // remove that child
+    nestedWhakapapa.children.splice(childIndex, 1)
   }
 
   return nestedWhakapapa
