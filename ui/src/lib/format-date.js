ab15953648b0395875aba88997c4e19a2ee02945
iantairea <waipuna@gmail.com>
2020-05-02 17:43:02 +1200

    playing with ui pasteboard.co/J6sDygK.png

--- ui/src/lib/format-date.js
+++ ui/src/lib/format-date.js
@@ -1,6 +1,6 @@
 export default function formatDate (bornAt) {
-  const months = ['Jan', 'Feb', 'Mar','Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-  let bornDate = new Date(bornAt)
-  let formattedDate = bornDate.getDate() + " " + months[bornDate.getMonth()] + " " + bornDate.getFullYear()
+  const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+  const bornDate = new Date(bornAt)
+  const formattedDate = bornDate.getDate() + ' ' + months[bornDate.getMonth()] + ' ' + bornDate.getFullYear()
   return formattedDate
 }
