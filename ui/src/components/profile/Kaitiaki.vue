57e66135e89cc09072137b36d4dfe8c0696f8c11
Ben <jamin.tairea@gmail.com>
2020-05-11 20:42:44 +0000

    added kaitiaki

--- ui/src/components/profile/Kaitiaki.vue
+++ ui/src/components/profile/Kaitiaki.vue
@@ -1,25 +1,25 @@
 <template>
   <v-card min-height="200px" light class="rounded-card" width="100%">
-    <h4 class="pa-3 mb-0"> {{ title }}</h4>
-    <v-card-text>
-      <p>{{ subtitle }}</p>
-    </v-card-text>
+    <v-tooltip left>
+      <template v-slot:activator="{ on }">
+        <h4 v-on="on" class="pa-3 mb-0"> {{ title }} </h4>
+      </template>
+      <span>{{ helperText }}</span>
+    </v-tooltip>
     <div>
-      <p style="color: lightgrey; text-align: center;">TODO: v-for</p>
+      <slot name="content"></slot>
     </div>
   </v-card>
 </template>
 
 <script>
+
 export default {
   name: 'Kaitiaki',
   props: {
     title: String,
     subtitle: String,
-    profiles: {
-      type: Array,
-      default: () => ([])
-    }
+    helperText: String
   }
 }
 </script>
