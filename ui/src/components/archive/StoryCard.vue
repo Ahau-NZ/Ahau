7ef3239a8617b277307e1a41f7b778bbbb03b596
Ben <jamin.tairea@gmail.com>
2020-05-23 23:01:31 +1200

    reverted carousel background color

--- ui/src/components/archive/StoryCard.vue
+++ ui/src/components/archive/StoryCard.vue
@@ -21,7 +21,7 @@
     <!-- ARTEFACT CAROUSEL -->
     <v-list-item v-if="story.artefacts && story.artefacts.length" class="px-0">
       <v-list-item-content :style="!mobile && !showArtefact && story.artefacts.length > 1 ? 'margin-bottom:-80px':''">
-        <v-carousel v-model="model" hide-delimiters :show-arrows="!mobile && fullStory" :show-arrows-on-hover="!mobile" :height="showArtefact ? 'auto' : mobile ? '300px' : '500px'" style="background-color:grey">
+        <v-carousel v-model="model" hide-delimiters :show-arrows="!mobile && fullStory" :show-arrows-on-hover="!mobile" :height="showArtefact ? 'auto' : mobile ? '300px' : '500px'">
           <v-carousel-item v-for="(artefact,i) in story.artefacts" :key="artefact.id">
             <Artefact :model="model" :index="i" @showArtefact="toggleShowArtefact($event)" :artefact="artefact" />
           </v-carousel-item>
