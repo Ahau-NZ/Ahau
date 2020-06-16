22d360e374cfd5d9977d54d9b45bfb5ee7a93f55
chereseeriepa <cherese.eriepa@outlook.com>
2020-06-17 10:28:43 +1200

    standard errors

--- ui/src/components/Avatar.stories.js
+++ ui/src/components/Avatar.stories.js
@@ -13,7 +13,7 @@ const ruru =
   'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Ruru-morepork-heaphy-track.jpg/800px-Ruru-morepork-heaphy-track.jpg'
 
 export const Image = () => ({
-  template: `<Avatar :image="image" alt="Ruru"/>`,
+  template: '<Avatar :image="image" alt="Ruru"/>',
   data: () => ({
     image: { uri: ruru }
   }),
@@ -21,7 +21,7 @@ export const Image = () => ({
 })
 
 export const Small = () => ({
-  template: `<Avatar :image="image" size="10vh" />`,
+  template: '<Avatar :image="image" size="10vh" />',
   data: () => ({
     image: { uri: ruru }
   }),
@@ -29,46 +29,46 @@ export const Small = () => ({
 })
 
 export const Koro = () => ({
-  template: `<Avatar gender="male" aliveInterval="1950-01-01/"/>`,
+  template: '<Avatar gender="male" aliveInterval="1950-01-01/"/>',
   components: { Avatar }
 })
 
 export const Kuia = () => ({
-  template: `<Avatar gender="female" aliveInterval="1950-01-01/"/>`,
+  template: '<Avatar gender="female" aliveInterval="1950-01-01/"/>',
   components: { Avatar }
 })
 
 export const Tane = () => ({
-  template: `<Avatar gender="male" aliveInterval="1950-01-01/"/>`,
+  template: '<Avatar gender="male" aliveInterval="1950-01-01/"/>',
   components: { Avatar }
 })
 
 export const Wahine = () => ({
-  template: `<Avatar gender="female" aliveInterval="1950-01-01/"/>`,
+  template: '<Avatar gender="female" aliveInterval="1950-01-01/"/>',
   components: { Avatar }
 })
 
 export const Tama = () => ({
-  template: `<Avatar gender="male" aliveInterval="1950-01-01/"/>`,
+  template: '<Avatar gender="male" aliveInterval="1950-01-01/"/>',
   components: { Avatar }
 })
 
 export const Kotiro = () => ({
-  template: `<Avatar gender="female" aliveInterval="1950-01-01/"/>`,
+  template: '<Avatar gender="female" aliveInterval="1950-01-01/"/>',
   components: { Avatar }
 })
 
 export const Unknown = () => ({
-  template: `<Avatar gender="unknown"/>`,
+  template: '<Avatar gender="unknown"/>',
   components: { Avatar }
 })
 
 export const NoGender = () => ({
-  template: `<Avatar aliveInterval="1950-01-01/"/>`,
+  template: '<Avatar aliveInterval="1950-01-01/"/>',
   components: { Avatar }
 })
 
 export const NoBornAt = () => ({
-  template: `<Avatar gender="male"/>`,
+  template: '<Avatar gender="male"/>',
   components: { Avatar }
 })
