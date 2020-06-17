d8a93921a201b939ab43701a1dca91f2e76c8a81
chereseeriepa <cherese.eriepa@outlook.com>
2020-06-17 15:11:00 +1200

    fixed typo

--- server/index.js
+++ server/index.js
@@ -44,7 +44,7 @@ module.exports = {
           },
           {
             typeDefs: whakapapa.typeDefs,
-            resolvers: whakapapa.rsesolvers
+            resolvers: whakapapa.resolvers
           }
         ]),
         context,
