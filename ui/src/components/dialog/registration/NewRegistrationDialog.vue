41755fb015c36cb3f29e419463a7d0b889d8c854
iantairea <waipuna@gmail.com>
2020-07-10 00:08:46 +1200

    tribe registration dialog

--- ui/src/components/dialog/registration/NewRegistrationDialog.vue
+++ ui/src/components/dialog/registration/NewRegistrationDialog.vue
@@ -2,63 +2,92 @@
   <Dialog :show="show" :title="title" @close="close" width="720px" :goBack="close" enableMenu>
     <template v-if="!hideDetails" v-slot:content>
       <v-col cols="12" :class="mobile ? 'pb-5 px-5' : 'px-5' ">
-        <v-hover v-if="errorMsgs && errorMsgs.length" v-slot:default="{ hover }">
-          <v-row @click="$emit('editProfile',formData)" align="center" style="border: 1px solid rgba(168,0,0); border-radius: 10px;" :style="hover ? 'cursor: pointer;background-color:rgba(168,0,0,0.1)':''">
-            <v-col cols="12" md="10">
-                <v-row>
-                  <span class="px-4 subtitle-2 secondary--text">To join this communtiy, please update the required areas on your profile</span>
-                </v-row>
-                <v-row v-for="error in errorMsgs" :key="error" justify="start" class="pl-12 py-1">
-                <span class="secondary--text subtitle-2"> - Please update your {{error}} information</span>
-                <!-- <v-col v-for="error in errorMsgs" :key="error[key]"></v-col> -->
-                </v-row>
-            </v-col>
-          </v-row>
-        </v-hover>
         <v-row>
-          <span class="py-6 px-4 subtitle-2 blue--text">To join this communtiy, please confirm that you are happy to share the following profile information with whanau members</span>
+          <span class="py-6 px-4 subtitle-2 blue--text">To join this communtiy, please confirm that you are happy to share the following profile information with <strong><i>{{currentProfile.preferredName}}</i></strong> members</span>
         </v-row>
-        <v-row>
-          <v-col cols="12" md="3" class="py-0">
-            <v-row class="justify-center pt-12">
-              <Avatar
-                class="big-avatar"
-                :size="mobile ? '200px':'100px'"
-                :image="formData.avatarImage"
-                :alt="formData.preferredName"
-                :gender="formData.gender"
-                :aliveInterval="formData.aliveInterval"
-                :deceased="formData.deceased"
-                @updateAvatar="formData.avatarImage = $event"
-              />
+        <!-- WRAP -->
+        <v-form ref="checkboxes">
+          <v-card outlined elevation='1' :style="checkbox1 ? 'border: 2px solid #4caf50' : '', mobile ? 'margin: 0px' : 'margin: 20px;' ">
+            <v-row>
+              <v-col cols="12" md="3" class="py-0">
+                <v-row class="justify-center pt-12">
+                  <Avatar
+                    class="big-avatar"
+                    :size="mobile ? '100px':'100px'"
+                    :image="formData.avatarImage"
+                    :alt="formData.preferredName"
+                    :gender="formData.gender"
+                    :aliveInterval="formData.aliveInterval"
+                    :deceased="formData.deceased"
+                    @updateAvatar="formData.avatarImage = $event"
+                  />
+                </v-row>
+              </v-col>
+              <v-col cols="12" md="9" sm="12" :align="mobile ? 'center' : 'start'" :order="mobile ? '3' : '1'" :class="!mobile ? 'pt-12':''">
+                <h1 class="primary--text" :style="mobile ? length: ''">{{ formData.legalName ? formData.legalName : formData.preferredName }}</h1>
+              </v-col>
             </v-row>
-          </v-col>
-          <v-col cols="12" md="9" sm="12" :align="mobile ? 'center' : 'start'" :order="mobile ? '3' : '1'" :class="!mobile ? 'pt-12':''">
-            <h1 class="primary--text" :style="mobile ? length: ''">{{ formData.legalName ? formData.legalName : formData.preferredName }}</h1>
-          </v-col>
-        </v-row>
-        <!-- <RegisterButton v-if="profile.type === 'community'" :class="!mobile ? 'margin-top':''"/> -->
-        <ProfileInfoCard :profile="formData" isRegistration/>
-        <ProfileCard>
-          <template v-slot:content>
-            <ProfileInfoItem title="About" smCols="12" mdCols="12" :value="formData.description"/>
-          </template>
-        </ProfileCard>
+            <!-- <RegisterButton v-if="profile.type === 'community'" :class="!mobile ? 'margin-top':''"/> -->
+            <ProfileInfoCard :profile="formData" isRegistration :style="mobile ? 'margin: 0px 10px' : 'margin: 0px 30px;'"/>
+            <ProfileCard :style="mobile ? 'margin: 10px 10px' : 'margin: 0px 30px;'">
+              <template v-slot:content>
+                <ProfileInfoItem title="About" smCols="12" mdCols="12" :value="formData.description"/>
+              </template>
+            </ProfileCard>  
+            
+            <v-divider></v-divider>
+            <v-card-actions style="display: flex; justify-content: center; align-items: center;">
+              <v-checkbox class="checkbox" color="success" v-model="checkbox1" :label="`I agree to share this information`" :rules="requiredRules"></v-checkbox> . 
+            </v-card-actions>
+          </v-card>
+      <!-- WRAP END -->
         <v-row>
-          <p class="py-6 px-4 subtitle-2 blue--text">The below private information will only be viewable by whanau kaitiaki</p>
+          <p class="py-6 px-4 subtitle-2 blue--text">The below private information will only be viewable by <strong><i>{{currentProfile.preferredName}}</i></strong> kaitiaki</p>
         </v-row>
-        <ProfileCard>
+        <!-- WRAP -->
+        <v-card outlined elevation='1' style="margin: 20px;  " :style="checkbox2 ? 'border: 2px solid #4caf50' : ''">
+        <ProfileCard style="margin: 30px;">
           <template v-slot:content>
             <v-row cols="12" class="pt-0" >
-              <ProfileInfoItem :class="mobile ? 'bb':'br'" smCols="12" mdCols="3" title="Date of birth" :value="dob"/>
-              <ProfileInfoItem :class="mobile ? 'bb':'br'" smCols="12" mdCols="3" title="Phone" :value="formData.phone"/>
-              <ProfileInfoItem :class="mobile ? 'bb':'br'" smCols="12" mdCols="3" title="Email" :value="formData.email"/>
-              <ProfileInfoItem smCols="12" mdCols="3" title="Address" :value="formData.address"/>
+              <ProfileInfoItem :class="mobile ? 'bb':'br bb'" smCols="12" mdCols="6" title="Date of birth" :value="dob"/>
+              <ProfileInfoItem :class="mobile ? 'bb':'bb'" smCols="12" mdCols="6" title="Phone" :value="formData.phone"/>
+              <ProfileInfoItem :class="mobile ? 'bb':'br'" smCols="12" mdCols="6" title="Email" :value="formData.email"/>
+              <ProfileInfoItem smCols="12" mdCols="6" title="Address" :value="formData.address"/>
             </v-row>
           </template>
         </ProfileCard>
+                   
+        <v-divider></v-divider>
+        <v-card-actions style="display: flex; justify-content: center; align-items: center;">
+          <v-checkbox class="checkbox" color="success" v-model="checkbox2" :label="`I agree to share this information`" :rules="requiredRules"></v-checkbox> . 
+        </v-card-actions>
+      </v-card>
+    </v-form>
+  <!-- WRAP END -->
+
       </v-col>
+
+      <v-hover v-if="errorMsgs && errorMsgs.length" v-slot:default="{ hover }">
+        <v-row @click="$emit('editProfile',formData)" align="center" style="border: 1px solid rgba(168,0,0); border-radius: 10px;" :style="hover ? 'cursor: pointer;background-color:rgba(168,0,0,0.1)':''">
+          <v-col cols="12">
+              <v-row justify="center">
+                <span class="px-4 subtitle-2 secondary--text">To join this communtiy, please update the required areas on your profile</span>
+              </v-row>
+              <v-row v-for="error in errorMsgs" :key="error" justify="center" class="py-1">
+                <span class="secondary--text "><i>- Please update your {{error}} information</i></span>
+              <!-- <v-col v-for="error in errorMsgs" :key="error[key]"></v-col> -->
+              </v-row>
+              <v-row class="pt-2" justify="center">
+                <v-icon color="secondary">mdi-account-edit</v-icon>
+                <v-btn text large>update your details</v-btn>
+              </v-row>
+          </v-col>
+        </v-row>
+      </v-hover>
+      
     </template>
+
+
 
 
     <!-- Actions Slot -->
@@ -70,9 +99,14 @@
         <!-- <v-icon color="secondary">mdi-close</v-icon> -->
         <span>cancel</span>
       </v-btn>
+      <!-- <v-btn 
+        @click="submit"
+        text large
+        class="blue--text mx-5"
+      > -->
       <v-btn 
         @click="submit"
-        :disabled="errorMsgs && errorMsgs.length"
+        :disabled="disabled"
         text large
         class="blue--text mx-5"
       >
@@ -106,7 +140,7 @@ import calculateAge from '@/lib/calculate-age'
 import pick from 'lodash.pick'
 import isEqual from 'lodash.isequal'
 import clone from 'lodash.clonedeep'
-import { mapActions } from 'vuex'
+import { mapActions, mapGetters } from 'vuex'
 
 const REQUIRED_ATTRS = [
   'id', 'legalName', 'aliveInterval', 'gender', 'relationshipType', 
@@ -196,37 +230,43 @@ export default {
   },
   data () {
     return {
+      checkbox1: false,
+      checkbox2: false,
       profile: {},
       formData: setProfileData(),
       hasSelection: false,
+      requiredRules: [
+       v => v == true || 'Please agree to share information'
+      ],
+      errorMsgs: []
     }
   },
   created () {
     this.getFullProfile()
   },
   computed: {
+    ...mapGetters(['currentProfile']),
     mobile () {
       return this.$vuetify.breakpoint.xs
     },
     customClass () {
       return 
     },
-    errorMsgs () {
-      var empty = {}
-      var output = Object.keys(this.formData)
-        .filter(key => REQUIRED_ATTRS.includes(key))
-        .reduce((obj, key) => {
-          obj[key] = this.formData[key];
-          return obj;
-        }, {});
-      
-      Object.entries(output).forEach(([key, value]) => {
-        if (isEmpty(output[key])) {
-          empty[key] = value
-        }
-      })
-      return Object.keys(empty)
-    }
+    disabled () {
+      this.getErrorMsgs()
+      if (this.errorMsgs && this.errorMsgs.length > 0) {
+        return true
+      }
+      return false
+    },
+    dob () {
+      if (this.formData.aliveInterval) {
+        console.log(this.formData.aliveInterval)
+        var formattedDate = formatDate(this.formData.aliveInterval)
+        return formattedDate
+      }
+      return ' '
+    },
     // profileChanges () {
     //   let changes = {}
     //   Object.entries(this.formData).forEach(([key, value]) => {
@@ -262,6 +302,23 @@ export default {
   // },
   methods: {
     ...mapActions(['setDialog']),
+    getErrorMsgs () {
+      // var errors = {}
+      var errors = []
+      var output = Object.keys(this.formData)
+        .filter(key => REQUIRED_ATTRS.includes(key))
+        .reduce((obj, key) => {
+          obj[key] = this.formData[key];
+          return obj;
+        }, {});
+      Object.entries(output).forEach(([key, value]) => {
+        if (isEmpty(output[key])) {
+          // errors[key] = value
+          errors.push(key)
+        }
+      })
+      this.errorMsgs = errors
+    },
     async getFullProfile () {
       const profile = await getRelatives(this.selectedProfile.id)
 
@@ -282,13 +339,7 @@ export default {
       }
       return formattedDate
     },
-    dob () {
-      if (this.formData.aliveInterval) {
-        var formattedDate = formatDate(this.formData.aliveInterval)
-        return formattedDate
-      }
-      return ' '
-    },
+    
     close () {
       this.$emit('close')
     },
@@ -311,13 +362,12 @@ export default {
     // },
 
     submit () {
-      this.checkSubmission()
-      console.log(this.errorMsgs)
-     
-
-      // if (!isEmpty(output)) {
-      //   this.$emit('submit', output)
-      // }
+      // this.checkSubmission()
+      // console.log(this.errorMsgs)
+  
+      if(this.$refs.checkboxes.validate()) {
+        console.log('approved.')
+      }
     }
   }
 }
@@ -344,5 +394,11 @@ export default {
 
 .v-input--radio-group__input label {
   font-size: 14px;
+}
+
+.checkbox /deep/ label {
+  /* color: #4caf50; */
+  /* font-weight: 600; */
+  font-size: 1.1em
 }
 </style>
