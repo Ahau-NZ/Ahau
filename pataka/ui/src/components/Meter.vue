4840dad64490c5d985bfb36af55e06a1ab4e21f6
luandro <luandro@gmail.com>
2020-07-25 11:09:47 -0300

    smaller meter line width

--- pataka/ui/src/components/Meter.vue
+++ pataka/ui/src/components/Meter.vue
@@ -4,7 +4,7 @@
       <v-card-text class="pt-0">
         <div class="title font-weight-light mb-2">{{title}}</div>
       </v-card-text>
-      <v-sparkline :labels="labels" :value="values" color="cyan" line-width="2" padding="16"></v-sparkline>
+      <v-sparkline :labels="labels" :value="values" color="cyan" line-width="1" padding="16"></v-sparkline>
     </v-sheet>
   </v-card>
 </template>
