bdf7b574990a03e0494bfe63bf0d83889eda0844
Ben <jamin.tairea@gmail.com>
2020-07-29 13:29:10 +1200

    updated default title

--- ui/src/components/dialog/registration/NewRegistrationDialog.vue
+++ ui/src/components/dialog/registration/NewRegistrationDialog.vue
@@ -290,7 +290,7 @@ export default {
   },
   props: {
     show: { type: Boolean, required: true },
-    title: { type: String, default: 'Create a new person' },
+    title: { type: String, default: 'Join Community' },
     hideDetails: { type: Boolean, default: false },
     readOnly: { type: Boolean, default: false },
     profile: { type: Object },
