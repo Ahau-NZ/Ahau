ca1d6b7b4d89a590dd454c6ea1d8f3a8b01458f8
chereseeriepa <cherese.eriepa@outlook.com>
2020-08-04 17:48:05 +1200

    fixed bug in dateRegex

--- ui/src/lib/csv.js
+++ ui/src/lib/csv.js
@@ -198,7 +198,7 @@ function isString (d) {
   return typeof d === 'string'
 }
 
-var dateRegex = /^(0?[1-9]|[12][0-9]|3[01])[/-](0?[1-9]|1[012])[/-]\d{4}$/
+var dateRegex = /^(0[1-9]|[12][0-9]|3[01])[/-](0?[1-9]|1[012])[/-]\d{4}$/
 
 function isValidDate (d) {
   return isEmpty(d) || dateRegex.test(d)
