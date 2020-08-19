78f4f6668dad6e1447e0f8072dc9327510eda7d5
mixmix <whimful@gmail.com>
2020-08-20 09:38:17 +1200

    first pass auto-lan connection

--- ssb-ahau/index.js
+++ ssb-ahau/index.js
@@ -1,14 +1,5 @@
-const { ApolloServer } = require('apollo-server-express')
-const { buildFederatedSchema } = require('@apollo/federation')
-const http = require('http')
-const express = require('express')
-const cors = require('cors')
-const Main = require('@ssb-graphql/main')
-const Tribes = require('@ssb-graphql/tribes')
-const Profile = require('@ssb-graphql/profile')
-const Whakapapa = require('@ssb-graphql/whakapapa')
-const Artefact = require('@ssb-graphql/artefact')
-const Story = require('@ssb-graphql/story')
+const pull = require('pull-stream')
+const graphqlServer = require('./graphql-server')
 
 module.exports = {
   name: 'ssb-ahau',
@@ -19,6 +10,7 @@ module.exports = {
     }
 
     graphqlServer(sbot)
+    lanConn(sbot)
   }
 }
 
@@ -35,52 +27,14 @@ function logPublish (sbot) {
   })
 }
 
-function graphqlServer (sbot) {
-  const PORT = 4000
-  const app = express()
-  app.options('*', cors())
-
-  const main = Main(sbot)
-  const tribes = Tribes(sbot)
-  const profile = Profile(sbot)
-  const story = Story(sbot)
-  const artefact = Artefact(sbot)
-  const whakapapa = Whakapapa(sbot, { ...profile.gettersWithCache, ...story.gettersWithCache, ...artefact.gettersWithCache })
-
-  main.loadContext((err, context) => {
-    if (err) throw err
-
-    const server = new ApolloServer({
-      schema: buildFederatedSchema([
-        main,
-        tribes,
-        profile,
-        artefact,
-        story,
-        whakapapa
-      ]),
-      context,
-      mockEntireSchema: false,
-      mocks: process.env.MOCK === true ? require('./mocks') : false
+function lanConn (sbot) {
+  pull(
+    sbot.lan.discoveredPeers(),
+    pull.through(console.log),
+    pull.drain(({ address, verified }) => {
+      pull.conn.connect(address, (err, data) => {
+        console.log(err, data)
+      })
     })
-    server.applyMiddleware({ app })
-
-    const httpServer = http.createServer(app)
-    server.installSubscriptionHandlers(httpServer)
-
-    httpServer.listen(PORT, () => {
-      console.log(
-        `🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`
-      )
-      console.log(
-        `🚀 Subscriptions ready at ws://localhost:${PORT}${server.subscriptionsPath}`
-      )
-      if (process.env.PLATFORM === 'cordova') {
-        require('cordova-bridge').channel.post(
-          'initialized',
-          JSON.stringify({ started: true })
-        )
-      }
-    })
-  })
+  )
 }
