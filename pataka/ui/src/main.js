5ac5978f19631aeb816eae93445c9394ca6c14dd
mixmix <whimful@gmail.com>
2020-08-27 14:33:04 +1200

    save pataka from first time race-condition

--- pataka/ui/src/main.js
+++ pataka/ui/src/main.js
@@ -1,21 +1,51 @@
 import Vue from 'vue'
+import gql from 'graphql-tag'
+import VuejsClipper from 'vuejs-clipper'
+
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
 import { createProvider } from './plugins/vue-apollo'
 import vuetify from './plugins/vuetify'
-import VuejsClipper from 'vuejs-clipper'
 
-Vue.use(VuejsClipper)
-Vue.config.productionTip = false
+main()
 
-new Vue({
-  router,
-  store,
+async function main () {
+  // install
+  Vue.use(VuejsClipper)
+  Vue.config.productionTip = false
 
-  apolloProvider: createProvider(),
-  vuetify,
+  const apolloProvider = createProvider()
 
-  render: h => h(App)
-}).$mount('#app')
+  checkReady(() => {
+    new Vue({
+      router,
+      store,
+
+      apolloProvider,
+      vuetify,
+
+      render: h => h(App)
+    }).$mount('#app')
+  })
+
+  async function checkReady (next) {
+    try {
+      await apolloProvider.defaultClient.query({
+        query: gql`{
+          whoami {
+            public {
+              feedId
+            }
+          }
+        }`
+      })
+    } catch (e) {
+      console.log('Waiting for Graphql')
+      return setTimeout(() => checkReady(next), 500)
+    }
+
+    next()
+  }
+}
