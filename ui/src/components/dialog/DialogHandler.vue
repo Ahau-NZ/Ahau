b858ea9d1d93fa70cd09c3af4194c94869a50069
luandro <luandro@gmail.com>
2020-08-27 11:57:07 -0300

    fix join community DialogHandler

--- ui/src/components/dialog/DialogHandler.vue
+++ ui/src/components/dialog/DialogHandler.vue
@@ -3,7 +3,7 @@
     <NewRegistrationDialog
       v-if="isActive('new-registration')"
       :show="isActive('new-registration')"
-      :title="dialogType === 'review' ? `Request to join : ${currentNotification.message.group.preferredName}` : `Request to join : ${currentNotification.message.group.preferredName} ---- ${currentNotification.message.outcome}`"
+      :title="dialogType === 'review' ? `Request to join : ${currentNotification.message.group.preferredName}` : `Request to join : ${currentTribe.public[0].preferredName}`"
       :profile="whoami.personal.profile"
       :parents.sync="parents"
       :parentIndex.sync="parentIndex"
