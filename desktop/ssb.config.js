da7325d38185150fccd6b469ab16e2e1e63e4dc6
mix irving <whimful@gmail.com>
2020-08-29 05:40:56 +0000

    seperate caps for each NODE_ENV, and new "staging" env

--- desktop/ssb.config.js
+++ desktop/ssb.config.js
@@ -2,13 +2,47 @@ const Config = require('ssb-config/defaults')
 const fs = require('fs')
 const path = require('path')
 
+const { NODE_ENV } = process.env
+const STAGING = 'staging'
+const DEVELOPMENT = 'development'
+
+if (NODE_ENV && NODE_ENV !== DEVELOPMENT && NODE_ENV !== STAGING) {
+  throw new Error(`Invalid NODE_ENV: "${NODE_ENV}"`)
+}
+
+let caps
+let appName
+switch (NODE_ENV) {
+  case DEVELOPMENT:
+    appName = 'ahau-dev'
+    caps = {
+      shs: 'BHjvWx+Z2GiHMPKxUXQn+AYCXPLsYkSVCoDfldJA9Fc=',
+      sign: 'tk6lgL0VpIhBotkcAlfyw+me4ysxY3bItUVwvIdiByI='
+    }
+    break
+
+  case STAGING:
+    appName = 'ahau-staging'
+    caps = {
+      shs: 'WPP7e6UeR6ur6bt/qxd1dPKGyXn1dqv2bDAeVTNXDuI=',
+      sign: 'zIHQqbxSpYeFEGnIWz2JaEHLpbHz2xd7if7f9fyOMJA='
+    }
+    break
+
+  default: // production
+    appName = 'ahau'
+    caps = {
+      shs: 'PW7NiFTuMDjzsNqZaWQ5gDUQdi8mHK2JcbZLx4HCyv8=',
+      sign: 'Peoj4NUl0I8EXp62EpKPcEcK28QGQ7tjk/vmyUOpzbY='
+    }
+}
+
 const customConfig = {
   port: 8087,
-  caps: {
-    shs: 'LftKJZRB4nbBRnlJuFteWG9AP+gGboVEhibx016bR0s='
-    // this is the "secret handshake" capability.
-    // only devices using this exact same code can talk to each other
-  },
+  caps,
+  // caps = capabilities, only apps with:
+  // - the same shs ("secret handshake") key can connect to each other
+  // - thas same sign can verify (+replicatie) messages with each other
   friends: { hops: 2 },
   lan: { legacy: false }, // disables legacy UDP announce (which doesn't respect caps.shs!)
   serveBlobs: {
@@ -23,10 +57,9 @@ const customConfig = {
 }
 
 module.exports = function () {
-  const appName = process.env.NODE_ENV === 'development'
-    ? 'ahau-dev'
-    : 'ssb-ahau'
-
+  if (NODE_ENV) {
+    console.log(`\x1b[37m\x1b[41m NODE_ENV \x1b[31m\x1b[47m ${process.env.NODE_ENV} \x1b[0m`)
+  }
   const config = Config(appName, customConfig)
 
   // write a copy of this config to ~/.{appName}/config
