b3b6c0bbfbbf07529daedefae0365175b81c3b74
Ian Tairea <waipuna@gmail.com>
2021-02-11 18:45:30 +1300

    new collections title ui

--- ui/src/views/CollectionShow.vue
+++ ui/src/views/CollectionShow.vue
@@ -1,41 +1,107 @@
 <template>
   <div>
     <v-container fluid v-if="collection">
-        <v-row v-if="!mobile && !showStory" style="width:30%;" class="">
-          <WhakapapaShowViewCard
-            type="collection"
-            :view="collection"
-            :access="access"
-            :shadow="false"
-          >
-            <template v-slot:edit v-if="collection && collection.canEdit">
-              <v-tooltip bottom>
-                <template v-slot:activator="{ on }">
-                  <v-btn
-                    v-on="on"
-                    @click.stop="dialog = 'edit-collection'"
-                    icon
-                    class="pa-0 px-3"
-                  >
-                    <v-icon small class="blue--text">mdi-pencil</v-icon>
-                  </v-btn>
-                </template>
-                <span>Edit Collection</span>
-              </v-tooltip>
-            </template>
-          </WhakapapaShowViewCard>
-        </v-row>
-        <v-row>
-          <v-col cols="12">
-            <Stories title="Stories" :stories="stories" @save="$emit('processStory', $event)" />
-          </v-col>
-        </v-row>
+      <!-- Collection Header -->
+      <v-row v-if="!showStory">
+        <v-col cols="12" xs="12" sm="12" md="9" class="pa-0">
+          <v-row>
+            <!-- Collection image -->
+            <!-- order (image first if not mobile) -->
+            <v-col :order="!mobile ? 1 : 2" cols="12" xs="12" sm="12" md="2">
+              <v-img :src="image" class="pa-0" cover height="100%" width="100%">
+              </v-img>
+            </v-col>
+            <!-- Collection title-->
+            <!-- order (title first if not mobile) -->
+            <v-col :order="!mobile ? 2 : 1" cols="12" xs="12" sm="12" md="9">
+                <v-row class="align-center pl-3">
+                  <h3 class="blue-grey--text text--darken-4">
+                    {{ collection.name }}
+                  </h3>
+                  <div v-if="!mobile && collection && collection.canEdit">
+                    <v-tooltip top>
+                      <template v-slot:activator="{ on }">
+                        <v-btn
+                          v-on="on"
+                          @click.stop="dialog = 'edit-collection'"
+                          icon
+                          class="pl-5"
+                        >
+                          <v-icon class="blue--text">mdi-pencil</v-icon>
+                        </v-btn>
+                      </template>
+                      <span>Edit Collection</span>
+                    </v-tooltip>
+                  </div>
+                </v-row>
+                <v-row v-if="mobile" class="justify-center">
+                  <div v-if="collection && collection.canEdit">
+                    <v-tooltip top>
+                      <template v-slot:activator="{ on }">
+                        <v-btn
+                          v-on="on"
+                          @click.stop="dialog = 'edit-collection'"
+                          icon
+                          
+                        >
+                          <v-icon class="blue--text">mdi-pencil</v-icon>
+                        </v-btn>
+                      </template>
+                      <span>Edit Collection</span>
+                    </v-tooltip>
+                  </div>
+                </v-row>
+                <div v-if="!mobile">
+                  <!-- DESKTOP: Description -->
+                  <p v-if="collection.description" class="black--text mb-0 py-2 caption">
+                    {{ collection.description }}
+                  </p>
+                  <!-- DESKTOP: # of stories -->
+                  <v-subheader v-if="collection.stories.length > 0" light class="pa-0">
+                    {{ collection.stories.length }}
+                    {{
+                      collection.stories.length == 1 ? " Story " : " Stories "
+                    }}
+                    in this Collection
+                  </v-subheader>
+                </div>
+ 
+            </v-col>
+            <v-col v-if="mobile" order="3">
+              <!-- MOBILE: Description -->
+              <p v-if="collection.description" class="black--text mb-0 py-2 caption">
+                {{ collection.description }}
+              </p>
+              <!-- MOBILE: # of stories -->
+              <v-subheader v-if="collection.stories.length > 0" light class="pa-0">
+                {{ collection.stories.length }}
+                {{ collection.stories.length == 1 ? " Story " : " Stories " }}
+                in this Collection
+              </v-subheader>
+            </v-col>
+          </v-row>
+          <v-divider class="black my-8"></v-divider>
+        </v-col>
+      </v-row>
+
+      <!-- STORIES -->
+      <v-row>
+        <v-col cols="12">
+          <Stories
+            title="Stories"
+            :stories="stories"
+            @save="$emit('processStory', $event)"
+          />
+        </v-col>
+      </v-row>
     </v-container>
+
     <!-- Dialog -->
     <NewCollectionDialog
-      v-if="collection && dialog === 'edit-collection'" :show="dialog === 'edit-collection'"
+      v-if="collection && dialog === 'edit-collection'"
+      :show="dialog === 'edit-collection'"
       :collection="collection"
-      :title="`Edit ${collection.name || 'Untitled' } Collection`"
+      :title="`Edit ${collection.name || 'Untitled'} Collection`"
       editing
       @submit="processCollection"
       @delete="dialog = 'delete-collection'"
@@ -51,34 +117,34 @@
 </template>
 
 <script>
-import { deleteCollection } from '@/lib/collection-helpers'
-import { getCollection } from '@/lib/story-helpers'
-import { getTribalProfile } from '@/lib/community-helpers'
+import { deleteCollection } from "@/lib/collection-helpers";
+import { getCollection } from "@/lib/story-helpers";
+import { getTribalProfile } from "@/lib/community-helpers";
 
-import mapProfileMixins from '@/mixins/profile-mixins.js'
-import { saveCollectionsMixin } from '@/mixins/collection-mixins.js'
+import mapProfileMixins from "@/mixins/profile-mixins.js";
+import { saveCollectionsMixin } from "@/mixins/collection-mixins.js";
 
-import WhakapapaShowViewCard from '../components/whakapapa/WhakapapaShowViewCard'
-import { mapGetters, mapMutations } from 'vuex'
-import Stories from '../components/archive/Stories.vue'
+import WhakapapaShowViewCard from "../components/whakapapa/WhakapapaShowViewCard";
+import { mapGetters, mapMutations } from "vuex";
+import Stories from "../components/archive/Stories.vue";
 
-import NewCollectionDialog from '@/components/dialog/archive/NewCollectionDialog.vue'
-import DeleteCollectionDialog from '@/components/dialog/archive/DeleteCollectionDialog.vue'
+import NewCollectionDialog from "@/components/dialog/archive/NewCollectionDialog.vue";
+import DeleteCollectionDialog from "@/components/dialog/archive/DeleteCollectionDialog.vue";
 
 export default {
-  name: 'CollectionShow',
+  name: "CollectionShow",
   // TODO: move to mixin
   mixins: [
     saveCollectionsMixin,
     mapProfileMixins({
-      mapMethods: ['getTribe']
+      mapMethods: ["getTribe"]
     })
   ],
   apollo: {
-    collection () {
+    collection() {
       return {
         ...getCollection(this.$route.params.collectionId)
-      }
+      };
     }
   },
   components: {
@@ -87,70 +153,90 @@ export default {
     NewCollectionDialog,
     DeleteCollectionDialog
   },
-  data () {
+  data() {
     return {
       collection: null,
       access: null,
       dialog: null
-    }
+    };
   },
   computed: {
-    ...mapGetters(['whoami', 'currentAccess', 'showStory']),
-    stories () {
-      if (!this.collection || !this.collection.stories) return []
+    ...mapGetters(["whoami", "currentAccess", "showStory"]),
+    stories() {
+      if (!this.collection || !this.collection.stories) return [];
 
       return this.collection.stories.map(link => {
         return {
           linkId: link.linkId,
           ...link.story
-        }
-      })
+        };
+      });
     },
-    mobile () {
-      return this.$vuetify.breakpoint.xs || this.$vuetify.breakpoint.sm
+    mobile() {
+      return this.$vuetify.breakpoint.xs || this.$vuetify.breakpoint.sm;
+    },
+    image() {
+      // if there is an image return it
+      if (
+        this.collection &&
+        this.collection.image &&
+        this.collection.image.uri
+      ) {
+        return this.collection.image.uri;
+      }
+
+      // otherwise return a default one
+      return this.type === "view" ? whakapapa : niho;
     }
   },
   watch: {
-    async collection (collection) {
+    async collection(collection) {
       if (collection.recps && collection.recps.length > 0) {
-        const tribe = await this.getTribe(collection.recps[0])
-        this.access = getTribalProfile(tribe, this.whoami)
-        this.setCurrentAccess(this.access)
+        const tribe = await this.getTribe(collection.recps[0]);
+        this.access = getTribalProfile(tribe, this.whoami);
+        this.setCurrentAccess(this.access);
       }
     }
   },
   methods: {
-    ...mapMutations(['setCurrentAccess']),
-    async processCollection ($event) {
-      const { stories } = $event
+    ...mapMutations(["setCurrentAccess"]),
 
-      await this.saveCollection($event)
-      await this.saveStoriesToCollection(this.collection, stories)
+    async processCollection($event) {
+      const { stories } = $event;
 
-      this.$parent.$apollo.queries.collections.refetch({ filter: { groupId: this.$route.params.tribeId } })
-      this.$parent.$apollo.queries.stories.refetch({ filter: { groupId: this.$route.params.tribeId } })
+      await this.saveCollection($event);
+      await this.saveStoriesToCollection(this.collection, stories);
 
-      this.$apollo.queries.collection.refetch()
+      this.$parent.$apollo.queries.collections.refetch({
+        filter: { groupId: this.$route.params.tribeId }
+      });
+      this.$parent.$apollo.queries.stories.refetch({
+        filter: { groupId: this.$route.params.tribeId }
+      });
+
+      this.$apollo.queries.collection.refetch();
     },
-    async deleteCollection () {
+    async deleteCollection() {
       const res = await this.$apollo.mutate(
         deleteCollection(this.collection.id, new Date())
-      )
+      );
 
       if (res.errors) {
-        console.error('failed to delete collection', res.errors)
-        return
+        console.error("failed to delete collection", res.errors);
+        return;
       }
 
       // reload parent component collections
-      await this.$parent.$apollo.queries.collections.refetch({ filter: { groupId: this.$route.params.tribeId } })
+      await this.$parent.$apollo.queries.collections.refetch({
+        filter: { groupId: this.$route.params.tribeId }
+      });
 
       // go to the default archive page
-      const [newPath] = this.$route.fullPath.split('archive/')
-      this.$router.push({ path: newPath + 'archive' }).catch(() => {})
+      const [newPath] = this.$route.fullPath.split("archive/");
+      this.$router.push({ path: newPath + "archive" }).catch(() => {});
     }
   }
-}
+};
 </script>
 <style lang="scss">
 @media (min-width: 1264px) and (max-width: 1903px) {
@@ -176,8 +262,12 @@ export default {
 }
 
 .niho-bg {
-  background: linear-gradient(to right, rgba(255, 255, 255, 0.99),
-  rgba(255, 255, 255, 0.7)), url(../assets/niho.svg);
+  background: linear-gradient(
+      to right,
+      rgba(255, 255, 255, 0.99),
+      rgba(255, 255, 255, 0.7)
+    ),
+    url(../assets/niho.svg);
   background-position-x: 100px;
   background-attachment: fixed;
   background-repeat: no-repeat;
@@ -190,14 +280,13 @@ export default {
   transition-timing-function: ease-in-out;
  } */
 
- .fade-enter-active {
-    transition: all 0.6s ease-in-out;
-  }
+.fade-enter-active {
+  transition: all 0.6s ease-in-out;
+}
 
-  .fade-enter,
-  .fade-leave-to {
-    opacity: 0;
-    transform: translateY(30px);
-  }
-
-</style>22px;
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+  transform: translateY(30px);
+}</style
+>22px;
