c36f550ac8f4e9d2a1fc3d69644240a1c87275e5
mixmix <whimful@gmail.com>
2021-04-13 22:06:55 +1200

    ensure video previews have content

--- ui/src/lib/get-video-poster.js
+++ ui/src/lib/get-video-poster.js
@@ -1,5 +1,8 @@
 const captureFrame = require('capture-frame')
 
+const INITIAL_PROPORTION = 0.3
+// https://www.dictionary.com/e/pop-culture/the-wadsworth-constant/
+
 export default function getVideoPoster (uri) {
   return new Promise((resolve, reject) => {
     const video = document.createElement('video')
@@ -15,22 +18,27 @@ export default function getVideoPoster (uri) {
       video.removeEventListener('canplay', onCanPlay)
       video.addEventListener('seeked', onSeeked)
 
-      video.currentTime = 2 // seek 2 seconds into the video
+      video.currentTime = Math.ceil(video.duration * INITIAL_PROPORTION) // seek into the video
     }
 
     function onSeeked () {
+      video.pause()
+      const frame = captureFrame(video)
+      const blob = new Blob([frame.image], { type: 'image/png' }) // eslint-disable-line
+
+      if (blob.size / 1024 < 50) { // if blob is too small, it's likely a blank screen
+        video.currentTime += 10
+        video.play()
+        return
+      }
+
       video.removeEventListener('seeked', onSeeked)
 
-      const frame = captureFrame(video)
-
       // unload video element, to prevent memory leaks
-      video.pause()
       video.src = ''
       video.load()
 
-      resolve(URL.createObjectURL(
-        new Blob([frame.image], { type: 'image/png' }) // eslint-disable-line
-      ))
+      resolve(URL.createObjectURL(blob))
     }
   })
 }
