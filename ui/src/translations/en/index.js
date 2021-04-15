201f488fed2a497b6c30eb9f271406931e48b908
BlakeAkapita <bakapita10@gmail.com>
2021-04-15 19:11:56 +1200

    Added translations to RecordForm

--- ui/src/translations/en/index.js
+++ ui/src/translations/en/index.js
@@ -1,6 +1,7 @@
 module.exports = {
   addPersonForm: require('./add-person-form.json'),
   addCommunityForm: require('./add-community-form.json'),
+  addStoryForm: require('./add-story-form.json'),
   pataka: require('./pataka'),
   viewPerson: require('./view-person.json'),
   viewTribes: require('./view-tribes.json')
