a66c5c3eb0fbf44c4558e661094f088fc0f89ca1
chereseeriepa <cherese.eriepa@outlook.com>
2021-05-19 16:05:01 +1200

    updated changelog, rm comments, update ssb-ahau

--- ui/src/plugins/vue-apollo.js
+++ ui/src/plugins/vue-apollo.js
@@ -2,8 +2,7 @@ import Vue from 'vue'
 import VueApollo from 'vue-apollo'
 import { ApolloClient, InMemoryCache } from '@apollo/client'
 import { createUploadLink } from 'apollo-upload-client' // partners with graphql-upload
-// import { createApolloClient, restartWebsockets } from 'vue-cli-plugin-apollo/graphql-client' // TODO rm
-import { IntrospectionFragmentMatcher } from 'apollo-cache-inmemory' // TODO rm ?
+import { IntrospectionFragmentMatcher } from 'apollo-cache-inmemory'
 
 import possibleTypes from './possibleTypes.json'
 
@@ -23,50 +22,6 @@ const AUTH_TOKEN = 'apollo-token'
 const httpEndpoint =
   process.env.VUE_APP_GRAPHQL_HTTP || `http://localhost:${env.ahau.graphql.port}/graphql`
 
-// Config
-// const defaultOptions = {
-//   // You can use `https` for secure connection (recommended in production)
-//   httpEndpoint,
-//   // You can use `wss` for secure connection (recommended in production)
-//   // Use `null` to disable subscriptions
-//   wsEndpoint: process.env.VUE_APP_GRAPHQL_WS || `ws://localhost:${env.ahau.graphql.port}/graphql`,
-//   // LocalStorage token
-//   tokenName: AUTH_TOKEN,
-//   // Enable Automatic Query persisting with Apollo Engine
-//   persisting: false,
-//   // Use websockets for everything (no HTTP)
-//   // You need to pass a `wsEndpoint` for this to work
-//   websocketsOnly: false,
-//   // Is being rendered on the server?
-//   ssr: false,
-
-//   // Override default apollo link
-//   // note: don't override httpLink here, specify httpLink options in the
-//   // httpLinkOptions property of defaultOptions.
-//   link: ApolloLink.from([
-//     // ...
-//     createUploadLink({
-//       uri: httpEndpoint // '/graphql'
-//       // opts: {
-//       //   mode: 'no-cors',
-//       // },
-//       // credentials: DEV ? "same-origin" : "include"
-//     })
-//   ])
-
-//   // Override default cache
-//   // cache: myCache
-
-//   // Override the way the Authorization header is set
-//   // getAuth: (tokenName) => ...
-
-//   // Additional ApolloClient options
-//   // apollo: { ... }
-
-//   // Client local data (see apollo-link-state)
-//   // clientState: { resolvers: { ... }, defaults: { ... } }
-// }
-
 // Call this in the Vue app file
 export function createProvider (options = {}) {
   // Create apollo client
@@ -78,7 +33,6 @@ export function createProvider (options = {}) {
     }),
     link: createUploadLink({ uri: httpEndpoint })
   })
-  // apolloClient.wsClient = wsClient
 
   // Create vue apollo provider
   const apolloProvider = new VueApollo({
@@ -109,7 +63,6 @@ export async function onLogin (apolloClient, token) {
   if (typeof localStorage !== 'undefined' && token) {
     localStorage.setItem(AUTH_TOKEN, token) // eslint-disable-line
   }
-  // if (apolloClient.wsClient) restartWebsockets(apolloClient.wsClient)
   try {
     await apolloClient.resetStore()
   } catch (e) {
@@ -123,7 +76,6 @@ export async function onLogout (apolloClient) {
   if (typeof localStorage !== 'undefined') {
     localStorage.removeItem(AUTH_TOKEN) // eslint-disable-line
   }
-  // if (apolloClient.wsClient) restartWebsockets(apolloClient.wsClient)
   try {
     await apolloClient.resetStore()
   } catch (e) {
