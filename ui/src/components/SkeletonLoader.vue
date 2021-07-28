fa0d88ff60e347c43bc60b487969d66fa6082fda
Cherese Eriepa <cherese.eriepa@outlook.com>
2021-07-28 02:35:11 +0000

    Pataka list skeleton

--- ui/src/components/SkeletonLoader.vue
+++ ui/src/components/SkeletonLoader.vue
@@ -10,7 +10,7 @@
       <v-row>
         <v-col v-for="n in totalSkeletons" :key="`skeleton-${n}`" :cols="cols">
           <v-skeleton-loader
-          :width="mobile ? '100%' : '87%'"
+          :width="skeletonWidth"
           :type="skeletonType"
           />
         </v-col>
@@ -23,17 +23,21 @@
 
 export default {
   name: 'SkeletonLoader',
-  components: {
-  },
   props: {
     totalSkeletons: [String, Number],
     skeletonType: String,
     cols: [String, Number],
-    customClass: String
+    customClass: String,
+    width: String
   },
   computed: {
     mobile () {
       return this.$vuetify.breakpoint.xs || this.$vuetify.breakpoint.sm
+    },
+    skeletonWidth () {
+      if (this.width) return this.width
+
+      return this.mobile ? '100%' : '87%'
     }
   }
 }
