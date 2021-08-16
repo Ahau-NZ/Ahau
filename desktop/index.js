87ca098f658db7ab266caaec50ec4f455f1a378d
mixmix <whimful@gmail.com>
2021-08-17 10:12:13 +1200

    tidy config, logging of karakia and startup info

--- desktop/index.js
+++ desktop/index.js
@@ -1,7 +1,11 @@
 const ahoy = require('ssb-ahoy')
 const env = require('ahau-env')()
+const chalk = require('chalk')
+const boxen = require('boxen')
 const { autoUpdater } = require('electron-updater')
+
 const Config = require('./ssb.config')
+const karakia = require('./karakia')
 
 const plugins = [
   'ssb-db',
@@ -33,25 +37,6 @@ const plugins = [
   'ssb-recps-guard'
 ]
 
-// Karakia tūwhera - dont not remove
-const karakia = `
----------------------------------
-Kia tau ngā manaakitanga a te mea ngaro
-ki runga ki tēnā, ki tēnā o tātou
-Kia mahea te hua mākihikihi
-kia toi te kupu, toi te mana, toi te aroha, toi te Reo Māori
-kia tūturu, ka whakamaua kia tīna! Tīna!
-Hui e, Tāiki e!
-
-Let the strength and life force of our ancestors
-Be with each and every one of us
-Freeing our path from obstruction
-So that our words, spiritual power, love, and language are upheld;
-Permanently fixed, established and understood!
-Forward together!
----------------------------------
-`
-
 const appURL = env.isDevelopment
   ? 'http://localhost:8080' // dev-server
   : `file://${__dirname}/dist/index.html` // production build
@@ -64,10 +49,35 @@ ahoy({
   // appDir: '../../AHAU/whakapapa-ora/desktop', // only use this when ssb-ahoy symlinked
   onReady: ({ config }) => {
     // this config has updated manifest added
-    console.log(karakia)
+
+    /* Karakia tūwhera */
+    karakia()
+
+    printConfig(config)
 
     if (env.isProduction) {
       autoUpdater.checkForUpdatesAndNotify()
     }
   }
 })
+
+function printConfig (config) {
+  const envName = env.isProduction ? '' : ` ${env.name.toUpperCase()} `
+
+  const configTxt = chalk`{blue AHAU} {white.bgRed ${envName}}
+
+{bold feedId}  ${config.keys.id}
+{bold path}  ${config.path}
+{bold network}
+  ├── host  ${config.host}
+  ├── port  ${config.port}
+  └── api   http://localhost:${config.graphql.port}/graphql
+`
+
+  console.log(boxen(configTxt, {
+    padding: 1,
+    margin: 1,
+    borderStyle: 'round',
+    borderColor: 'blue'
+  }))
+}
