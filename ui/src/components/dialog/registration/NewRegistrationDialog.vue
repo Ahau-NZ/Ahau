b2f308b8722ed5335f3c7c574837e3500e55df9a
chereseeriepa <cherese.eriepa@outlook.com>
2022-08-19 11:25:30 +1200

    improve profile saving and loading during registrations

--- ui/src/components/dialog/registration/NewRegistrationDialog.vue
+++ ui/src/components/dialog/registration/NewRegistrationDialog.vue
@@ -65,26 +65,37 @@
                   outlined
                 >
                   <v-card-text>
-                    <div v-if="isValidPersonalProfile">
-                      No updates required. Go to your profile if you wish to provide more information
+                    <div v-if="isLoadingProfile">
+                      Please wait while your profile is updating
+                      <v-progress-circular
+                        indeterminate
+                        color="#b12526"
+                      />
                     </div>
                     <div v-else>
-                      Please update the following information on your profile: <br>
-                      <ul>
-                        <li v-for="({ prop }) in missingRequiredFields" :key="prop">
-                          {{ prop }}
-                        </li>
-                      </ul>
+                      <div v-if="isValidPersonalProfile">
+                        No updates required. Go to your profile if you wish to provide more information
+                      </div>
+                      <div v-else>
+                        Please update the following information on your profile: <br>
+                        <ul>
+                          <li v-for="({ prop }) in missingRequiredFields" :key="prop">
+                            {{ prop }}
+                          </li>
+                        </ul>
+                      </div>
                     </div>
                   </v-card-text>
                 </v-card>
                 <v-btn
                   color="primary"
+                  :disabled="isLoadingProfile"
                   @click="goProfile"
                 >
                   Go to edit profile
                 </v-btn>
                 <v-btn
+                  :disabled="!isValidPersonalProfile || isLoadingProfile"
                   class="ml-5"
                   color="primary"
                   @click="step = hasJoiningQuestions ? 2 : 3"
@@ -455,12 +466,16 @@ export default {
   },
   computed: {
     ...mapGetters(['whoami']),
+    ...mapGetters('alerts', ['alertSettings']),
     ...mapGetters('tribe', ['currentTribe', 'tribeJoiningQuestions', 'tribeCustomFields', 'tribeRequiredFields', 'tribeDefaultFields']),
     personalProfile () {
       return {
         ...this.whoami.personal.profile,
         customFields: this.rawCustomFields
       }
+    },
+    isLoadingProfile () {
+      return this.alertSettings.delay === -1
     },
     displayName () {
       return this.personalProfile.preferredName
