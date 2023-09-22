9e80311b0f432accccbeca6d5102e624408aef9b
mixmix <mix@protozoa.nz>
2023-09-22 16:45:27 +1200

    update ENV to work with Vite

--- ui/vue.config.js
+++ ui/vue.config.js
@@ -1,24 +1,3 @@
-const dotenv = require('dotenv')
-const webpack = require('webpack')
-const { join } = require('path')
-
-// TODO make sure this is working
-// I think VITE handles this quite differently!
-const env = dotenv.config({ path: join(__dirname, '.env') })
-
-if (env.error) {
-  if (env.error.message.match(/no such file or directory/)) {
-    if (process.env.NODE_ENV === 'production') throw env.error
-    // eslint-disable-next-line no-console
-    else console.error('No ui/.env file found, please set one up')
-  } else {
-    throw env.error
-  }
-} else {
-  process.env.VUE_APP_MIXPANEL_TOKEN = env.parsed.MIXPANEL_TOKEN
-  process.env.VUE_APP_MIXPANEL_API_HOST = env.parsed.MIXPANEL_API_HOST
-}
-
 process.env.VUE_APP_PLATFORM = process.env.PLATFORM === 'cordova'
   ? 'cordova'
   : 'web'
@@ -37,25 +16,5 @@ module.exports = {
     apollo: {
       lintGQL: true
     }
-  },
-  configureWebpack: {
-    // webpack 5 removed default-on polyfills for many Node modules
-    // the following config is about providing browser-friendly replacements
-
-    resolve: {
-      fallback: {
-        path: require.resolve('path-browserify'),
-        http: require.resolve('stream-http')
-      }
-    },
-    plugins: [
-      new webpack.DefinePlugin({
-        'global.Date': 'globalThis.Date' // required for edtf
-      }),
-      new webpack.ProvidePlugin({
-        Buffer: ['buffer', 'Buffer'],
-        process: 'process/browser'
-      })
-    ]
   }
 }
