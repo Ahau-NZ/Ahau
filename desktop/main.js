ed4072531180103787b576363f0cab948dd9721f
mix irving <--get>
2023-10-25 16:01:22 +1300

    update ahoy, electron > 26.4.1

--- desktop/main.js
+++ desktop/main.js
@@ -1,3 +1,4 @@
+/* eslint-disable camelcase */
 const ahoy = require('ssb-ahoy')
 const env = require('ahau-env')()
 const chalk = require('chalk')
@@ -6,6 +7,18 @@ const level = require('level')
 const { join } = require('path')
 const { app } = require('electron')
 const { autoUpdater } = require('electron-updater')
+
+// WARNING monkey patch! --------------------------------------
+const na = require('sodium-native')
+na.sodium_malloc = function sodium_malloc_monkey_patched (n) {
+  return Buffer.alloc(n)
+}
+// Electron > 20.3.8 breaks a napi method that `sodium_malloc`
+// depends on to create external buffers. (see v8 memory cage)
+//
+// This crashes electron when called by various libraries, so
+// we monkey-patch this particular function.
+// ------------------------------------------------------------
 
 const config = require('./ssb.config')()
 const karakia = require('./karakia')
