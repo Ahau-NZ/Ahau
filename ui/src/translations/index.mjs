f79cd7fbd5bdbb67f59baeed225cc5cf58c43a6f
mixmix <mix@protozoa.nz>
2024-02-19 11:52:30 +1300

    add Dutch NL+SR

--- ui/src/translations/index.mjs
+++ ui/src/translations/index.mjs
@@ -3,6 +3,8 @@ import * as en_NZ from './en_NZ/index.mjs'
 import * as en_US from './en_US/index.mjs'
 import * as es_ES from './es_ES/index.mjs'
 import * as mi_NZ from './mi_NZ/index.mjs'
+import * as nl_NL from './nl_NL/index.mjs'
+import * as nl_SR from './nl_SR/index.mjs'
 import * as pt_BR from './pt_BR/index.mjs'
 
 export default {
@@ -10,5 +12,7 @@ export default {
   en_US,
   es_ES,
   mi_NZ,
+  nl_NL,
+  nl_SR,
   pt_BR
 }
